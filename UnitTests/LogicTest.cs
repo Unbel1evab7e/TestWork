5699cca27d70aa2257c5d1effe0558155762977c
Unbel1evab7e <unbel1evab7e@gmail.com>
2021-07-15 15:16:56 +0700

    future add unit test

---
+++ UnitTests/LogicTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestWork.Logic;
+using Xunit;
+
+namespace UnitTests
+{
+    public class LogicTest
+    {
+        CurrencyConverter converter = new CurrencyConverter();
+        [Fact]
+        public void  DivideByZeroExceptionPass()
+        {
+            decimal reuslt = converter.CalculateConvert(0, "RUB", "EUR");
+            Assert.Equal(0, reuslt);
+        }
+    }
+}
